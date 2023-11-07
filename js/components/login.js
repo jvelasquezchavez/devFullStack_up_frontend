59f3f1729027b5745dd53eaf945cf1dcada216a8
Juan Pablo Velasquez <jvelasquez@bizlogit.net>
2023-11-06 23:20:59 -0300

    Agrego pegarle al endpoint de login

--- js/components/login.js
+++ js/components/login.js
@@ -1,6 +1,12 @@
 const loginButton = document.getElementById('login-button');
+const loginForm = document.getElementById('formLogin');
 
-loginButton.addEventListener('click', () => {
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("aa");
+});
+
+loginForm.addEventListener('submit', async function(e){
+    e.preventDefault();
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
@@ -8,6 +14,29 @@ loginButton.addEventListener('click', () => {
         alert('Por favor, complete el usuario.');
     else if (!password)
         alert('Por favor, complete la contraseña.');
-    else
-        window.location.href = `../views/home.html?username=${encodeURIComponent(username)}`;
-});
+    else{
+        const result = await login(username, password);
+        console.log(result);
+        if (!result.hasError)
+            window.location.href = `../views/home.html?username=${encodeURIComponent(username)}`;
+        else
+            alert(result.data);
+    }
+}); 
+
+const login = async (username, password)=>{
+    const data = {
+        email: username,
+        password: password,
+    };
+    
+    const requestOptions = {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json',},
+        body: JSON.stringify(data),
+    };
+
+    let result = await fetch('http://localhost:5000/endp/auth/login', requestOptions);
+
+    return await result.json();
+}
